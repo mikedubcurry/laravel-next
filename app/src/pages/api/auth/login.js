ac43bbaa41883b9952c5962c98bd24460cff30a9
mikedubcurry <michaelcurry95@gmail.com>
2023-04-08 15:40:09 -0400

    working on auth with laravel

--- app/src/pages/api/auth/login.js
+++ app/src/pages/api/auth/login.js
@@ -1,27 +1,26 @@
+import axios from "axios";
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     // Process a POST request
     const { email, password } = req.body;
     // send request to laravel api
-    const response = await fetch("http://localhost/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    });
-    if (!response.ok) {
-      // return error
-      return res
-        .status(response.status)
-        .json({ message: "Something went wrong" });
+    const response = await axios
+      .get("http://localhost/sanctum/csrf-token")
+      .then(async (response) => {
+        let res = await axios.post("http://localhost/api/login", {
+          email,
+          password,
+        });
+        return res;
+      });
+
+    if (response.status === 200) {
+      // set cookie
+      res.setHeader("Set-Cookie", response.headers["set-cookie"]);
     }
-    const data = await response.json();
-    // return response
-    return res.status(response.status).json(data);
+
+    return res.status(response.status).json(response.data);
   } else {
     // return bad request
     return res.status(400).json({ message: "Bad request" });
