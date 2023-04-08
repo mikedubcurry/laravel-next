049215f746c25c0e2ca9a3d5ccae786dc5428cd6
mikedubcurry <michaelcurry95@gmail.com>
2023-04-08 15:40:30 -0400

    working on auth with laravel

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Auth::routes();
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', 'App\Http\Controllers\AuthController@login');
